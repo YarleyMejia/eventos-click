66f7d74213182ac544334ae259106cb0aec196c8
ycmejia <yarleyc.mejiab@uqvirtual.edu.co>
2025-09-04 05:22:16 -0500

    Se realizan ajustes previs a implementacion del despliegue.

---
+++ src/test/java/co/edu/uniquindio/proyecto/test/CuentaServicioTest.java
@@ -0,0 +1,50 @@
+package co.edu.uniquindio.proyecto.test;
+
+import co.edu.uniquindio.proyecto.excepciones.cuenta.CuentaNoActivadaException;
+import co.edu.uniquindio.proyecto.modelo.documentos.Cuenta;
+import co.edu.uniquindio.proyecto.modelo.enums.EstadoCuenta;
+import co.edu.uniquindio.proyecto.modelo.vo.CodigoValidacion;
+import co.edu.uniquindio.proyecto.repositorios.CuentaRepo;
+import co.edu.uniquindio.proyecto.servicios.interfaces.CuentaServicio;
+import co.edu.uniquindio.proyecto.dto.CrearCuentaDTO;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class CuentaServicioTest {
+
+    @Autowired
+    private CuentaServicio cuentaServicio;
+
+    @Autowired
+    private CuentaRepo cuentaRepo;
+
+    @Test
+    public void crearCuentaTest(){
+    // Crear un DTO con los datos para crear una nueva cuenta
+        CrearCuentaDTO crearCuentaDTO = new CrearCuentaDTO(
+                    "123",
+                    "Pepito perez",
+                    "12121",
+                    "Calle 123",
+                    "pepitoperez@email.com",
+                    "password"
+        );
+        // Se espera que no se lance ninguna excepción
+        assertDoesNotThrow( () -> {
+            //Se crea la cuenta y se imprime el id
+            String id = cuentaServicio.crearCuenta(crearCuentaDTO);
+            //Se espera que el id no sea nulo
+            assertNotNull(id);
+            } );
+    }
+}
+
---
+++ src/test/java/co/edu/uniquindio/proyecto/test/EventoServicioTest.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.proyecto.test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EventoServicioTest {
+}
